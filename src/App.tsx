2ae7f9e900ee675a779874267c0c6e1eca5d2f17
ItsukiArakaki268 <itukiprog@gmail.com>
2025-12-28 01:22:22 +0900

    Material UIを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import {
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  TextField,
+} from "@mui/material";
+
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Box>
+        <TextField
+          label="TODO"
+          variant="outlined"
+          placeholder="入力してください"
+        />
+        <Button>追加</Button>
+        <List>
+          <ListItem>
+            あ<IconButton>あ</IconButton>
+          </ListItem>
+        </List>
+      </Box>
     </>
   );
 }
