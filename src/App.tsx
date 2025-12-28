38f1f9cb18d5e40ce3055eb4a83ae665a809e349
ItsukiArakaki268 <itukiprog@gmail.com>
2025-12-28 13:40:28 +0900

    テキストボックスとボタンを実装

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,25 @@ import {
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <Box>
-        <TextField
-          label="TODO"
-          variant="outlined"
-          placeholder="入力してください"
-        />
-        <Button>追加</Button>
+      <h1>TODO</h1>
+      <Box
+        sx={{
+          maxWidth: 600,
+          margin: "auto",
+          padding: 2,
+          backgroundColor: "grey.100",
+          borderRadius: 1.5,
+        }}
+      >
+        <Box sx={{ display: "flex", gap: 1, mb: 2 }}>
+          <TextField
+            label="TODO"
+            variant="outlined"
+            placeholder="入力してください"
+            fullWidth
+          />
+          <Button variant="contained">追加</Button>
+        </Box>
         <List>
           <ListItem>
             あ<IconButton>あ</IconButton>
